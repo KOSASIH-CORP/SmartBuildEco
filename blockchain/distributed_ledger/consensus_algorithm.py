36f4311a9e970b094d8da976bf60405800f26f78
KOSASIH <kosasihg88@gmail.com>
2024-07-10 02:09:39 +0700

    Create consensus_algorithm.py

---
+++ blockchain/distributed_ledger/consensus_algorithm.py
@@ -0,0 +1,13 @@
+import random
+
+class ConsensusAlgorithm:
+    def __init__(self):
+        pass
+
+    def validate_block(self, block):
+        # Validate the block using the consensus algorithm
+        pass
+
+    def add_block(self, block):
+        # Add the block to the ledger
+        pass
